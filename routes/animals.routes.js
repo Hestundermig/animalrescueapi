768d5fcc6ddaca1cc02b17562599fbd6a4ee285e
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-03-30 10:49:48 +0200

    insomnia commit

--- routes/animals.routes.js
+++ routes/animals.routes.js
@@ -3,21 +3,58 @@ const router = express.Router()
 const Animal = require("../models/animals.model")
 const auth = require("../authMiddleware")
 
+// router.get("/animals", async function (request, response, next) {
+//     try {
+//         let result = await Animal.find();
+//         return response.status(200).json(result)
+//     } catch (error) {
+//         return next(error)
+//     }
+// });
+
+// router.get("/animals/:id", auth, async function (request, response, next) {
+//     try {
+//         let result = await Animal.findById(request.params.id);
+//         return response.status(200).json(result)
+//     } catch (error) {
+
+//     }
+// })
+
+
 router.get("/animals", async function (request, response, next) {
+
+    let offset = parseInt(request.query.offset) || 0 // || = eller
+    let limit = parseInt(request.query.limit) || 5
+
     try {
-        let result = await Animal.find();
-        return response.status(200).json(result)
+        let count = (await Animal.find()).length;
+        let results = await Animal.find().skip(offset).limit(limit);
+
+        let queryStringNext = `?offset=${offset+limit}&limit=${limit} `
+        let queryStringPrev = null;
+
+        if (offset > 0 ) {
+            queryStringPrev = 
+            `?offset=${offset-limit}&limit=${limit}`
+        }
+
+        let apiUrl = `${request.protocol}://${request.hostname}${request.hostname==='localhost' ? ":4000" : ' ' }`
+        let apiPath = `${request.baseUrl}${request.path}`
+
+        console.log(request.originalUrl)
+
+        let output = {
+            count,
+            next: ( offset + limit < count ) ? apiUrl + apiPath + queryStringNext : null,
+            previous: offset > 0 ? apiUrl + apiPath + queryStringPrev : null,
+            results: results,
+            url: `${apiUrl}${apiPath}`
+        }
+
+        return response.status(200).json(output)
     } catch (error) {
         return next(error)
-    }
-});
-
-router.get("/animals/:id", auth, async function (request, response, next) {
-    try {
-        let result = await Animal.findById(request.params.id);
-        return response.status(200).json(result)
-    } catch (error) {
-
     }
 });
 
